4821f7f8f01f750159cfc8900ca5eea229124914
kartikb7 <50640114+kartikb7@users.noreply.github.com>
2019-10-07 22:59:05 -0400

    Update weatherDisplay.py

--- weatherDisplay.py
+++ weatherDisplay.py
@@ -22,7 +22,7 @@ def weatherInfo(Date,City):
     # Creating date variables for 3 days before and after the game
     weatherDates = []
     weatherDates.append((datetime.strptime(Date, '%Y/%m/%d')- timedelta(days=3)).strftime('%m-%d-%Y'))
-    if Date<'12-28-2019':
+    if datetime.strptime(Date, '%Y/%m/%d')<datetime(2019, 12, 28):
         weatherDates.append((datetime.strptime(Date, '%Y/%m/%d')+ timedelta(days=3)).strftime('%m-%d-%Y'))
     else:
         weatherDates.append('12-31-2019')
